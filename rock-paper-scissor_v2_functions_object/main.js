be613f23d672ab37ad6803fa5a7eed9840526060
Xeetez <pdlkshitiz@gmail.com>
2024-01-09 20:39:38 +1100

    localstore

--- rock-paper-scissor_v2_functions_object/main.js
+++ rock-paper-scissor_v2_functions_object/main.js
@@ -1,10 +1,27 @@
-const score = {
-  wins: 0,
-  losses: 0,
-  ties: 0
-};
+let score = JSON.parse(localStorage.getItem('score'));
 
+//checking if score is null
+// can check as if score === null or !score same thing
+if(!score){
+  score = {
+    wins: 0,
+    losses: 0,
+    ties: 0
+  }
 
+}
+
+console.log();
+
+function resetScore(){
+  score.wins = 0;
+  score.losses = 0;
+  score.ties = 0;
+  //reset local store
+  localStorage.removeItem('score');
+
+  alert(`Reset Done....\nWins: ${score.wins} Losses: ${score.losses} Ties: ${score.ties}`); 
+}
 
 function playGame(playerMove){
   const computerMove = pickComputerMove();
@@ -23,18 +40,23 @@ function playGame(playerMove){
     result = 'You Win';
   }
 
+  updateResult(playerMove, computerMove, result);
+  
+}
+function updateResult(playerMove, computerMove, result){
   if(result === 'You Win'){
     score.wins += 1;
   }else if(result === 'You Lose'){
     score.losses += 1;
   }else if(result === 'Tie'){
     score.ties +=1;
-  }
-  //display result
-  alert(`You picked: ${playerMove}.\nComputer picked: ${computerMove}.\nResult: ${result}\n\nWins: ${score.wins} Losses: ${score.losses} Ties: ${score.ties}`);
-  
-}
+  } 
+  //saving score on localstorage
 
+  localStorage.setItem('score', JSON.stringify(score));
+
+  alert(`You picked: ${playerMove}.\nComputer picked: ${computerMove}.\nResult: ${result}\n\nWins: ${score.wins} Losses: ${score.losses} Ties: ${score.ties}`);
+}
 function pickComputerMove(){
   const randomNumber = Math.random();
   let computerMove = '';
@@ -50,12 +72,7 @@ function pickComputerMove(){
   return computerMove;
 }
 
-function resetScore(){
-  score.wins = 0;
-  score.losses = 0;
-  score.ties = 0;
-  alert(`Reset Done....\nWins: ${score.wins} Losses: ${score.losses} Ties: ${score.ties}`); 
-}
+// JSON
 
-const jsonString = JSON.stringify(score);
-console.log(JSON.parse(jsonString));
+// const jsonString = JSON.stringify(score);
+// console.log(JSON.parse(jsonString));
